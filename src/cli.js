37322b80dddb193f78bc2f6dfd2b4d5bcfa1362c
Armine Palanjyan <a.p.t2@mail.ru>
2026-01-10 22:52:02 +0400

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,9 +3,9 @@ import readlineSync from 'readline-sync';
 const greeting = () => {
   console.log ('Welcome to the Brain Games!')
 
-  const name = readlineSync.question('May I have your name?: ')
+  const name = readlineSync.question('May I have your name? ')
 
-  console.log (`Hello, ${name}`)
+  console.log (`Hello, ${name}!`)
 }
 
 
